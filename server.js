253b842ce667a1597ca066b79def4625f9708184
Eshaan Bansal <eshaanbansal2005@gmail.com>
2026-02-07 04:32:22 +0530

    updated html

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+const cookieParser = require('cookie-parser');
 const express = require('express');
 const app = express();
 
 app.use(express.static('./public'))
+app.use(cookieParser());
+
 
 app.listen(3000, () => {
     console.log('Listening on port 3000')
